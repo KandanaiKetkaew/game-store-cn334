1fde39b719bd661658cfe7767dfb12cf95d5def7
Kandanai <arting.kk@gmail.com>
2024-04-28 19:03:27 +0700

    fix whitenoise

--- game_store/settings.py
+++ game_store/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'app_general.apps.AppGeneralConfig',
     'app_gamelist.apps.AppGamelistConfig',
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
