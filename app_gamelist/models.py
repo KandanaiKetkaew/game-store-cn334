e44a558eddf9d51e13ef1b8d33cafebbdf64d06c
Kandanai <arting.kk@gmail.com>
2024-04-29 03:09:47 +0700

    add genre

--- app_gamelist/models.py
+++ app_gamelist/models.py
@@ -6,3 +6,4 @@ class Game(models.Model):
     initial_release = models.DateTimeField(null=True)
     description = models.TextField(null=True)
     image_relative_url = models.CharField(max_length=50,null=True,blank=True)
+    genre = models.CharField(max_length=60,blank=True)
